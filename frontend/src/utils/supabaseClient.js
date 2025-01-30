d1e41467db813aacf1383b7898fd817cb02d94fb
steven88ly <steven88ly@gmail.com>
2025-01-30 18:25:04 -0500

    Added supabase client connection and DOM routes to login/register/home/dashboard

---
+++ frontend/src/utils/supabaseClient.js
@@ -0,0 +1,7 @@
+import {createClient} from "@supabase/supabase-js"
+
+const supabaseURL = "https://rdnotelsaojvscgmfdve.supabase.co"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJkbm90ZWxzYW9qdnNjZ21mZHZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgyNzU0NjksImV4cCI6MjA1Mzg1MTQ2OX0.3wH9FUglUQ-wy9w5pXX2FqWCp6j-_gqL25_zK2O3AIk"
+
+const supabase = createClient(supabaseURL, supabaseAnonKey);
+export default supabase
