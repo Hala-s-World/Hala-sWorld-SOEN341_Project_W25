716e5f895b2aa822254edbe8dc2bd55d861a3190
Hala Belamri <100871287+Hala-Belamri@users.noreply.github.com>
2025-02-06 18:06:49 -0500

    Implement homepage structure, CSS styling, and animations (US#14)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Register from "./assets/components/Register";
+import Homepage from "./pages/Homepage";
 import LoginPage from "./pages/LoginPage";
 import Dashboard from "./pages/Dashboard";
 import Wrapper from "./pages/Wrapper";
@@ -9,10 +9,13 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Login */}
-        <Route path="/" element={<LoginPage />} />
-       
-        {/* dashboard */}
+        {/* Homepage as default */}
+        <Route path="/" element={<Homepage />} />
+
+        {/* Authentication */}
+        <Route path="/login" element={<LoginPage />} />
+
+        {/* Dashboard */}
         <Route
           path="/dashboard"
           element={
