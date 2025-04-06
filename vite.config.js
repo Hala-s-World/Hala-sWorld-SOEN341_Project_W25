a8340e40cc80992eeedf2b730695b848096f98df
Hala Belamri <100871287+hlbels@users.noreply.github.com>
2025-04-05 20:44:44 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
@@ -13,13 +12,5 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './tests/setup.js'
-  },
-  resolve: {
-    alias: [
-      {
-        find: /\.css$/,
-        replacement: path.resolve(__dirname, './tests/__mocks__/style.js')
-      }
-    ]
   }
 })
